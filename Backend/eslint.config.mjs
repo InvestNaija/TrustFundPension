84fe249d1016a6e56da0ac06c4e9c05fe9a5bbbc
Abimbola Hassan <AHassan@chapelhilldenham.com>
2025-03-18 11:15:04 +0100

    Changed to nestjs

--- Backend/eslint.config.mjs
+++ Backend/eslint.config.mjs
@@ -1,28 +1,35 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+// @ts-check
+import eslint from '@eslint/js';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
-
-export default [
-   {ignores: ["dist/"]},
-   {files: ["**/*.{js,mjs,cjs,ts}"]},
-   {languageOptions: { globals: globals.node }},
-   pluginJs.configs.recommended,
-   ...tseslint.configs.recommended,
-   {
-      rules: {
-         camelcase: "error",
-         "no-const-assign": "off",
-         "no-underscore-dangle": ["error", { "allow": ["__"] }],
-         "class-methods-use-this": "off", // Optional, turn off if you don"t require class methods to use "this"
-         "no-multi-assign": "off", // Optional, turn off if you allow multiple assignments in a single line
-         "no-unused-vars": ["error", { "argsIgnorePattern": "^_" }],
-         indent: [
-            "warn", 3
-         ],
-         semi: [
-            "error", "always"
-         ],
-         "consistent-return": "error"
-      },}
-];
+export default tseslint.config(
+  // {
+  //   ignores: ['eslint.config.mjs'],
+  // },
+  // eslint.configs.recommended,
+  // ...tseslint.configs.recommendedTypeChecked,
+  // eslintPluginPrettierRecommended,
+  // {
+  //   languageOptions: {
+  //     globals: {
+  //       ...globals.node,
+  //       ...globals.jest,
+  //     },
+  //     ecmaVersion: 5,
+  //     sourceType: 'module',
+  //     parserOptions: {
+  //       projectService: true,
+  //       tsconfigRootDir: import.meta.dirname,
+  //     },
+  //   },
+  // },
+  // {
+  //   rules: {
+  //     '@typescript-eslint/no-explicit-any': 'off',
+  //     '@typescript-eslint/no-floating-promises': 'warn',
+  //     '@typescript-eslint/no-unsafe-argument': 'warn'
+  //   },
+  // },
+);
--- Backend/src/database/sequelize/INv2/index.ts
+++
@@ -1,7 +0,0 @@
-import { Sequelize, Op } from "sequelize";
-export { Sequelize, Op };
-export * from "./models/BvnData";
-export * from "./models/Tenant";
-export * from "./models/User";
-export * from "./models/Role";
-export * from "./models/TenantUserRole";

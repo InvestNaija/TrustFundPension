95ac8884aa0c6bb76f7bbd3dd12293043cc2306c
jemikalah123 <okeoghene.philip5@gmail.com>
2025-04-21 10:27:23 +0100

    fix test cases

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -7,6 +7,7 @@ import {
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './core/database';
 import { UserModule } from './modules/user';
+import { PensionModule} from './modules/pension';
 import { ThirdPartyServicesModule } from './modules/third-party-services';
 import { AppService } from './app.service';
 
@@ -19,7 +20,8 @@ import { AppService } from './app.service';
     }),
     UserModule,
     DatabaseModule,
-    ThirdPartyServicesModule
+    ThirdPartyServicesModule,
+    PensionModule
 
   ],
   controllers: [AppController],
